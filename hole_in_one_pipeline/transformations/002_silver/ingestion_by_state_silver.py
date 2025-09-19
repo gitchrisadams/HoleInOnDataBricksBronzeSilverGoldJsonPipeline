c9a4f9de1859e976b799b7dcd4cd248e149883fc
gitchrisadams <gitchrisadams>
2025-09-19 19:47:16 +0000

    Removal of columns not needed in silver table

--- hole_in_one_pipeline/transformations/002_silver/ingestion_by_state_silver.py
+++ hole_in_one_pipeline/transformations/002_silver/ingestion_by_state_silver.py
@@ -12,11 +12,19 @@ def hole_in_one_silver():
     # Silver Layer Transformations
     df = spark.read.option("multiline", "true").json("/Volumes/hole_in_one/hole_in_one_schema/hole_in_one_volume/data_bronze.json") \
         .withColumn("first_name", upper("first_name"))
+    
+    columns_to_drop = [
+        "age_group", "ball_used", "club_brand", "club_used", "comments", 
+        "course_country", "course_id", "course_state", "createdAt", 
+        "date_of_hole_in_one_date", "gender", "golfers_country", "handicap", "id",
+        "number_of_hios", "regdate", "updatedAt", "witness_one", "witness_two", "witness_three", "years_golfing"                       
+    ]
+    df_cleaned = df.drop(*columns_to_drop)
 
     # Write new silver json file to Volume
     single_file_output_path = "/Volumes/hole_in_one/hole_in_one_schema/hole_in_one_volume/data_silver"
     new_output_path = "/Volumes/hole_in_one/hole_in_one_schema/hole_in_one_volume/data_silver.json"
-    df.coalesce(1).write.format("json").mode("overwrite").save(single_file_output_path)
+    df_cleaned.coalesce(1).write.format("json").mode("overwrite").save(single_file_output_path)
 
     # Logic to get json into one file
     list_files = dbutils.fs.ls(single_file_output_path)
@@ -24,4 +32,4 @@ def hole_in_one_silver():
     dbutils.fs.mv(json_file_path, new_output_path)
     dbutils.fs.rm(single_file_output_path, True)
 
-    return df
+    return df_cleaned
